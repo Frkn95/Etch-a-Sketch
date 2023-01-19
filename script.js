81f2b6c9da9312d21828474099424b49db021497
Frkn95 <106371671+Frkn95@users.noreply.github.com>
2023-01-19 22:53:34 +0100

    added a random color logic, still need implement

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const container = document.getElementById("container")
-
+const rainbowBtn = document.querySelector(".rainbowBtn")
+const blackBtn = document.querySelector(".blackBtn")
+const randomWarmColors = document.querySelector(".randomWarmColors")
+const randomColdColors = document.querySelector(".randomColdColors")
 
 function makeGrid(rows,cols){
     for (let i = 0; i < (rows * cols); i++) {
@@ -9,6 +12,21 @@ function makeGrid(rows,cols){
     }
 }
 
-
+function getRandomColor(){
+  const randomHue = Math.floor(Math.random() * 360)
+  const randomColor = `hsl(${randomHue},100%,50%)`
+  return randomColor
+}
 
 makeGrid(16,16)
+
+// const square = document.querySelector(".square")
+
+// square.addEventListener("mouseover", () => setColor(square))
+
+// function setColor(element){
+//   const color = getRandomColor()
+//   element.style.background = color;
+//   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+// }
+
