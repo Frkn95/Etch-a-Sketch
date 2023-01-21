739ca97b4809bd860d297c5f705dd7bc1ef929e8
Frkn95 <106371671+Frkn95@users.noreply.github.com>
2023-01-21 11:47:33 +0100

    Finished Projects, later more functions

--- script.js
+++ script.js
@@ -1,45 +1,92 @@
-const container = document.getElementById("container")
-const smallBtn = document.querySelector(".smallBtn");
-const mediumBtn = document.querySelector(".mediumBtn")
-const largeBtn = document.querySelector(".largeBtn")
-const xLargeBtn = document.querySelector(".xLargeBtn")
 
-let paintOnGrid = false
-document.body.addEventListener("onmousedown",() => paintOnGrid = true)
-document.body.addEventListener("onmouseup",() => paintOnGrid = false)
+const input = document.querySelector('input');
+const rangeText = document.getElementById('rangetext')
+const sketchArea = document.querySelector(`#sketcharea`);
+const resetButton = document.getElementById('btn__reset');
+const blackWhiteButton = document.getElementById('btn__bw');
 
-function makeGrid(rowsCols){
-  container.style.gridTemplateColumns = `repeat(${rowsCols}, 1fr)`
-  container.style.gridTemplateRows = `repeat(${rowsCols}, 1fr)`
 
-  for (let i = 0; i < (rowsCols * rowsCols); i++) {
-  let square = document.createElement("div")
-  square.className = "square"
-  container.appendChild(square) 
-  }
+function createElem() {
+    let newElem = document.createElement('div');
+    newElem.classList.add('griditem');
+    sketchArea.appendChild(newElem);
 }
 
-function reloadGrid(prefVal){
-  container.innerHTML =""
-  makeGrid(prefVal)
+function deleteElem() {
+    for (let i = sketchArea.childNodes.length; i > 0; i--) {
+        sketchArea.innerHTML = '';
+    }
 }
 
-smallBtn.addEventListener("click", (prefVal) => {
-  prefVal = 8
-  reloadGrid(prefVal)
-})
-mediumBtn.addEventListener("click", (prefVal) => {
-  prefVal = 16
-  reloadGrid(prefVal)
-})
-largeBtn.addEventListener("click", (prefVal) => {
-  prefVal = 32
-  reloadGrid(prefVal)
-})
-xLargeBtn.addEventListener("click", (prefVal) => {
-  prefVal = 64
-})
-
-window.onload = () => {
-  makeGrid(16)
+function updateGrid() {
+    if (input.value == 1) {
+        deleteElem();
+        sketchArea.setAttribute('style', 'grid-template-columns: repeat(4, 1fr);');
+        rangeText.textContent = "4";
+        for (let i = 0; i < 16; i++) {
+            createElem();
+        }
+        addElemBlackStyle();
+    } else if (input.value == 2) {
+        deleteElem();
+        sketchArea.setAttribute('style', 'grid-template-columns: repeat(8, 1fr);');
+        rangeText.textContent = "8";
+        for (let i = 0; i < 64; i++) {
+            createElem();
+        }
+        addElemBlackStyle();
+    } else if (input.value == 3) {
+        deleteElem();
+        sketchArea.setAttribute('style', 'grid-template-columns: repeat(16, 1fr);');
+        rangeText.textContent = "16";
+        for (let i = 0; i < 256; i++) {
+            createElem();
+        }
+        addElemBlackStyle()
+    } else if (input.value == 4) {
+        deleteElem();
+        sketchArea.setAttribute('style', 'grid-template-columns: repeat(32, 1fr);');
+        rangeText.textContent = "32";
+        for (let i = 0; i < 1024; i++) {
+            createElem();
+        }
+        addElemBlackStyle();
+    }
 }
+let colorValue = '';
+function colorRandomizer() {
+    let randomNumber = Math.random();
+
+    if (randomNumber < 0.25) {
+        colorValue = '#e8e8e8';
+    } else if (randomNumber < 0.5) {
+        colorValue = '#f05454';
+    } else if (randomNumber < 0.75) {
+        colorValue = '#30475e';
+    } else {
+        colorValue = '#222831';
+    }
+}
+
+function addElemBlackStyle() {
+    const gridItem = document.querySelectorAll('.griditem');
+
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].addEventListener('mouseover', () => {
+            colorRandomizer()
+            gridItem[i].style.backgroundColor = colorValue;
+        });
+    }
+}
+
+updateGrid(); // called to update the function upon initial page load
+
+input.addEventListener('change', updateGrid)
+
+
+resetButton.addEventListener('click', () => {
+    const gridItem = document.querySelectorAll('.griditem');
+    for (let i = 0; i < gridItem.length; i++) {
+        gridItem[i].style.backgroundColor = `white`;
+    }
+});
